09992c0e6364d4dc59d0cfb1c1eab98aa056c2b9
JillKlatt <klatt.jill@gmail.com>
2021-03-22 13:23:19 -0400

    Add login and signup forms

--- character-base/app/controllers/characters_controller.rb
+++ character-base/app/controllers/characters_controller.rb
@@ -2,4 +2,11 @@ require './config/environment'
 
 class CharactersController < Sinatra::Base
 
+    get '/characters' do 
+        redirect_if_not_logged_in
+
+        @characters = current_user.characters
+        erb :'characters/index'
+    end
+
 end
--- character-base/app/controllers/users_controller.rb
+++ character-base/app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ require './config/environment'
 
 class UsersController < Sinatra::Base
 
+    get '/create_user' do
+        redirect_if_already_logged_in
+        erb :'users/new'
+    end
+
+    post '/create_user' do
+        redirect_if_already_logged_in
+        user = User.new(params[:user])
+            if user.save 
+                session["user_id"] = user.id 
+                redirect '/characters'
+            else
+                redirect '/signup'
+            end
+    end
 end
