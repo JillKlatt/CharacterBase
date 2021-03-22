e6624157931ef0724b7856d95aef54ff61a3957b
JillKlatt <klatt.jill@gmail.com>
2021-03-22 17:36:41 -0400

    Add login routes

--- character-base/app/controllers/users_controller.rb
+++ character-base/app/controllers/users_controller.rb
@@ -23,6 +23,22 @@ class UsersController < ApplicationController
     end
 
     get '/login' do
+        redirect_if_already_logged_in
+        erb :'users/login'
     end
+
+    post '/login' do 
+        redirect_if_already_logged_in
+        user = User.find_by(email: params[:user][:email])
+
+            if user && user.authenticate(params[:user][:password])
+                session["user_id"] = user.id 
+                redirect '/characters'
+            else
+                redirect '/login'
+            end
+    end
+
+
 
 end
