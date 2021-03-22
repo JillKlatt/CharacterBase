39c5ce2be07929361ce33d373ff35a056d46d030
JillKlatt <klatt.jill@gmail.com>
2021-03-22 14:38:28 -0400

    Add show and edit to character controller

--- character-base/app/controllers/characters_controller.rb
+++ character-base/app/controllers/characters_controller.rb
@@ -9,4 +9,29 @@ class CharactersController < Sinatra::Base
         erb :'characters/index'
     end
 
+    get '/characters/new' do
+        erb :'characters/new'
+    end
+
+    post '/characters/new' do
+        @character = Character.create(params)
+        redirect to "/characters/#{@character.id}"
+    end
+
+    get '/characters/:id' do 
+        @character = Character.find_by_id(params[:id])
+        erb :show
+    end
+
+    patch '/characters/:id' do
+        @character = Character.find_by_id(params[:id])
+        @character.name = params[:name]
+        @character.age = params[:age]
+        @character.race = params[:race]
+        @character.class = params[:class]
+        @character.special_abilities = params[:special_abilities]
+        @cahracter.description = params[:description]
+        @character.save
+        redirect to "/characters/#{@character.id}"
+    end
 end
