b019e020853414ddc0ed0351c8fcfd7ade1521b5
JillKlatt <klatt.jill@gmail.com>
2021-03-22 17:31:00 -0400

    Add login view

--- character-base/app/controllers/users_controller.rb
+++ character-base/app/controllers/users_controller.rb
@@ -7,18 +7,18 @@ class UsersController < ApplicationController
     end
 
     get '/create_user' do
-        #redirect_if_already_logged_in
+        redirect_if_already_logged_in
         erb :'users/new'
     end
 
     post '/create_user' do
-        #redirect_if_already_logged_in
+        redirect_if_already_logged_in
         user = User.new(params[:user])
             if user.save 
                 session["user_id"] = user.id 
                 redirect '/characters'
             else
-                #redirect '/signup'
+                redirect '/create_user'
             end
     end
 
