4f0599095320abaadd06c58a8556299e4d2d455d
JillKlatt <klatt.jill@gmail.com>
2021-03-23 15:19:19 -0400

    Moved login/logout to new sessions_controller class

--- character-base/app/controllers/sessions_controller.rb
+++ character-base/app/controllers/sessions_controller.rb
@@ -1,9 +1,27 @@
 class SessionsController < ApplicationController
 
-# get '/login' do
+    get '/login' do
+        redirect_if_already_logged_in
+        erb :'users/login'
+    end
 
-#     redirect_if_already_logged_in
-#     erb :'sessions/new'
+    post '/login' do 
+        redirect_if_already_logged_in
+        user = User.find_by(email: params[:user][:email])
 
-# end
+            if user && user.authenticate(params[:user][:password])
+                session["user_id"] = user.id 
+                redirect '/characters'
+            else
+                #False (Flash message, incorrect password)
+                redirect '/login'
+            end
+    end
+
+    get '/logout' do
+        redirect_if_not_logged_in
+        session.delete("user_id")
+        redirect '/login'
+    end
+
 end
--- character-base/app/controllers/users_controller.rb
+++ character-base/app/controllers/users_controller.rb
@@ -18,33 +18,9 @@ class UsersController < ApplicationController
                 session["user_id"] = user.id 
                 redirect '/characters'
             else
+                #Flash Message: Please try again
                 redirect '/create_user'
             end
     end
-
-    get '/login' do
-        redirect_if_already_logged_in
-        erb :'users/login'
-    end
-
-    post '/login' do 
-        redirect_if_already_logged_in
-        user = User.find_by(email: params[:user][:email])
-
-            if user && user.authenticate(params[:user][:password])
-                session["user_id"] = user.id 
-                redirect '/characters'
-            else
-                redirect '/login'
-            end
-    end
-
-    get '/logout' do
-        redirect_if_not_logged_in
-        session.delete("user_id")
-        redirect '/login'
-    end
-
-
 
 end
