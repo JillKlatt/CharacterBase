386b0029ca4d71bb3d2fbe3fa2863dd9255db434
JillKlatt <klatt.jill@gmail.com>
2021-03-23 11:48:33 -0400

    Add user_id as reference to table

--- character-base/db/schema.rb
+++ character-base/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_154438) do
+ActiveRecord::Schema.define(version: 2021_03_23_154615) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_03_23_154438) do
     t.string "class_name"
     t.string "special_abilities"
     t.text "description"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
